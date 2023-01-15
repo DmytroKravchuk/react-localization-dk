e75a12409a120c1320a609b58bff2bc4b39d8bb0
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2023-01-15 21:58:16 +0200

    updated structure for publishing

---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo, useState, useContext } from 'react'
+
+type Props = {
+  localization: any
+  children: any
+  defaultValue: string
+  abbreviations?: Array<string>
+}
+
+const LocalizationContext = React.createContext({
+  currentLocalization: null,
+  setCurrentAbbreviation: () => {},
+  currentAbbreviation: null,
+  abbreviations: [],
+})
+
+export const LocalizationProvider = ({ localization, children, defaultValue, abbreviations }: Props) => {
+  const [currentAbbreviation, setCurrentAbbreviation] = useState(defaultValue)
+
+  const valueObj = useMemo(
+    () => ({
+      currentLocalization: localization[currentAbbreviation],
+      setCurrentAbbreviation,
+      currentAbbreviation,
+      abbreviations,
+    }),
+    [currentAbbreviation, abbreviations, localization],
+  )
+
+  return (
+    // @ts-ignore
+    <LocalizationContext.Provider value={valueObj}>{children}</LocalizationContext.Provider>
+  )
+}
+
+export const useLocalization = useContext(LocalizationContext)
